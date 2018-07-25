a8c6dd2a4bf9dd6579fd99d59d692e03b8f9c6ca
Rían Errity <rian@paradaux.ie>
2018-07-25 03:53:28 +0100

    TYPOO!!!

--- modules/commands.js
+++ modules/commands.js
@@ -20,7 +20,7 @@ module.exports.purge = async function purge(message, args, client) {
     const fetched = await message.channel.fetchMessages({limit: args[2]}); 
     message.channel.bulkDelete(fetched)
         .catch(error => message.channel.send(`Error: ${error}`));
-    config.log(fetched);
+    console.log(fetched);
     log("MODERATOR COMMAND", "User deleted `"  + args[2] + "` messages using " + config.prefix + "mod purge.", message, client)
 }
 
